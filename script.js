780606a7b006f446dd8cf48ecd4bd215d18ce5f9
Lornz18 <mellgodwinbarza22@gmail.com>
2025-03-27 15:45:02 +0800

    updating files

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const toggleMenu = document.querySelector(".toggle__menu");
-const headerNav = document.querySelector(".header__nav");
-const backDrop = document.querySelector(".backDrop");
-toggleMenu.addEventListener("click", () => {
-    toggleMenu.classList.toggle("open");
-    headerNav.classList.toggle("open");
-    backDrop.classList.toggle("open");
-})
+// const toggleMenu = document.querySelector(".toggle__menu");
+// const headerNav = document.querySelector(".header__nav");
+// const backDrop = document.querySelector(".backDrop");
+// toggleMenu.addEventListener("click", () => {
+//     toggleMenu.classList.toggle("open");
+//     headerNav.classList.toggle("open");
+//     backDrop.classList.toggle("open");
+// })
